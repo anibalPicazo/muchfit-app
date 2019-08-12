aa76592a1ca3c89f13e71539163b5b4e1e9c90c6
Anibal <anibal.picazo@hotmail.com>
2019-08-13 00:27:28 +0200

    form login
    No hace nada al hacer el login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -5,15 +5,19 @@
                     <h1 class="text-center mb-8 secondary--text">Acceso</h1>
                     <form>
                         <v-text-field
+                                v-model="username"
                                 outlined
                                 label="Usuario"
+                                required
                         ></v-text-field>
                         <v-text-field
+                                v-model="password"
                                 type="password"
                                 label="Contraseña"
                                 outlined
+                                required
                         ></v-text-field>
-                        <v-btn block large color="primary">Acceder</v-btn>
+                        <v-btn block large color="primary" @click="submit">Acceder</v-btn>
                     </form>
                 </v-flex>
             </v-layout>
@@ -21,18 +25,46 @@
 </template>
 
 <script>
-
+  import {required} from 'vuelidate/lib/validators'
 
   export default {
     name: "login",
-        data() {
+    data() {
       return {
         username: '',
         password: ''
       }
+
+
+    },
+    validations: {
+      username: {
+        required
+      },
+      password: {
+        required
+
+      }
     },
     methods: {
+      submit() {
+        this.$v.$touch();
+        if(this.$v.$error) this.login();
 
+      },
+      login: async function () {
+        try {
+          let data = await this.$auth.loginWith('local', {
+            data: {
+              username: this.username,
+              password: this.password
+            }
+          })
+        } catch (e) {
+          console.log(e)
+          this.error = e.response.data.message
+        }
+      }
     }
   }
 </script>
