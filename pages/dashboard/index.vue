8e4d1c3eb6a2be3bdf0b4d9c0b1a0457b763b1ae
Anibal <anibal.picazo@hotmail.com>
2019-10-28 17:55:16 +0100

    stats

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -2,7 +2,7 @@
   <div>
       <v-container >
         <v-row >
-         <v-col >
+         <v-col md="7" >
               <v-card-title> Comidas recomendadas</v-card-title>
            <v-row justify="space-between" class="mx-auto">
              <v-col md="3"  sm="6">
@@ -84,11 +84,71 @@
 
              </v-col>
            </v-row>
-           <v-row>
-             <v-card elevation="5">
+           <v-card elevation="3">
+           <v-row class="mx-auto">
+             <v-col md="6" sm="6">
+               <v-row>
+                 <div class="text-center ma-12">
+                   Entrenamientos realizados
+                 <v-progress-circular
+                   :rotate="45"
+                   :size="49"
+                   :value="42"
+                   :width="12"
+                   color="light-blue"
+                 >32</v-progress-circular>
+                 </div>
+               </v-row>
+               <v-row>
+                 <div class="text-center ma-12">
+                   Entrenamientos realizados:
+                   <v-progress-circular
+                     :rotate="45"
+                     :size="49"
+                     :value="42"
+                     :width="12"
+                     color="pink lighten-1"
+                   >32</v-progress-circular>
+                 </div>
+               </v-row>
+             </v-col>
+             <v-col md="6" sm="6">
+               <v-card
+                 class="mx-auto text-center"
+                 dark
+                 max-width="600"
+               >
+                 <v-card-text>
+                   <v-sheet >
+                     <v-sparkline
+                       :value="value"
+                       color="rgba(255, 255, 255, .7)"
+                       height="100"
+                       padding="24"
+                       stroke-linecap="round"
+                       smooth
+                     >
+                       <template v-slot:label="item">
+                         {{ item.value }} Kg
+                       </template>
+                     </v-sparkline>
+                   </v-sheet>
+                 </v-card-text>
 
-             </v-card>
+                 <v-card-text>
+                 <div class="display-1 font-weight-thin">Indice de mejora </div>
+               </v-card-text>
+
+                 <v-divider></v-divider>
+
+                 <v-card-actions class="justify-center">
+                   <v-btn block text>Go to Report</v-btn>
+                 </v-card-actions>
+               </v-card>
+             </v-col>
            </v-row>
+           </v-card>
+
          </v-col>
           <v-col>
 
@@ -100,7 +160,17 @@
 
 <script>
     export default {
-        name: "dashboard"
+        data: () => ({
+            value: [
+                56,
+                61,
+                62,
+                57,
+                55,
+                66,
+                71,
+            ],
+        }),
     }
 </script>
 
