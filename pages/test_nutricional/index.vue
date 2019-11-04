9c3dcedcacd1760509d201609618f15ccbd10a6b
Anibal <anibal.picazo@hotmail.com>
2019-11-04 18:38:29 +0100

    two layouts

--- pages/test_nutricional/index.vue
+++ pages/test_nutricional/index.vue
@@ -1,50 +1,77 @@
 <template>
   <v-container fluid>
-    <v-card>
-    <v-row>
-      <v-col md="12" sm="12">
-        <v-card-title>
-          <span class="headline">Test de entrenamiento</span>
-        </v-card-title>
-        <v-card-text>
-          <v-col cols="12" sm="6" md="4">
-            <v-text-field label="Legal first name*" required></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6" md="4">
-            <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6" md="4">
-            <v-text-field
-              label="Legal last name*"
-              hint="example of persistent helper text"
-              persistent-hint
-              required
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field label="Email*" required></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-text-field label="Password*" type="password" required></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-select
-              :items="['0-17', '18-29', '30-54', '54+']"
-              label="Age*"
-              required
-            ></v-select>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-autocomplete
-              :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-              label="Interests"
-              multiple
-            ></v-autocomplete>
-          </v-col>
-        </v-card-text>
+    <v-row >
+      <v-col sm="12" md="6">
+        <v-card class="mt-5" elevation="10">
+          <v-sheet
+            class="v-sheet--offset ml-4"
+            color="blue"
+            elevation="8"
+            max-width="calc(100% - 500px)"
+            height="40px"
+          >
+            <v-icon large color="white">mdi-progress-clock </v-icon><span style="color: white"> Experiencia</span>
+          </v-sheet>
+          <v-row>
+            <v-col md="12" sm="12">
+              <v-card-text>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field label="Legal first name*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-autocomplete
+                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-col>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col sm="12" md="6">
+        <v-card class="mt-5" elevation="10" >
+          <v-sheet
+            class="v-sheet--offset ml-4"
+            color="blue"
+            elevation="8"
+            max-width="calc(100% - 500px)"
+            height="40px"
+          >
+            <v-icon large color="white">mdi-progress-clock </v-icon><span style="color: white"> Frecuencia de entrenamiento</span>
+          </v-sheet>
+          <v-row>
+            <v-col md="12" sm="12">
+              <v-card-text>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field label="Legal first name*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-autocomplete
+                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-col>
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+
       </v-col>
     </v-row>
-    </v-card>
+
   </v-container>
 </template>
 
@@ -55,5 +82,8 @@
 </script>
 
 <style scoped>
-
+  .v-sheet--offset {
+    top: -24px;
+    position: relative;
+  }
 </style>
