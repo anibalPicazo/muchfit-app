c0d2042baf1ac190f9253a6c5a97a39eed5d2574
anibal.picazo <anibal.picazo@securitasdirect.es>
2023-12-19 17:26:26 +0100

    url

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,7 +87,7 @@ const config = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: process.env.API_URL || 'https://be-water-my-plant-backend-production.up.railway.app/'
+    baseURL: process.env.API_URL || 'https://much-fit-app-production.up.railway.app/'
   },
   /*
   ** vuetify module configuration
